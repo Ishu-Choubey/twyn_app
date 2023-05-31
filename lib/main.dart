154ecc259901693283fb1f964d319447496f0729
Ishu <choubeyishu.2503@gmail.com>
2023-05-31 17:15:17 +0530

    scrollable shiz

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget
         fontFamily: 'Helvetica'
       ),
       title: 'Tywn Login Page',
-      home: LoginPage(),
+      home: LoginPage(), //entering login page
     );
   }
 }
