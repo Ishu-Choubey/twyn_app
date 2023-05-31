092ed7bb28351d5d0412444a314bdafe1565c8f9
EVILxASHUTOSH <dearashutoshmishra2002@gmail.com>
2023-05-31 15:56:40 +0530

    submitted page

--- lib/Screen/submitted-page.dart
+++ lib/Screen/submitted-page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:myapp/utils.dart';
+import 'package:twyn_app/utils.dart';
 
 class Scene extends StatelessWidget {
   @override
